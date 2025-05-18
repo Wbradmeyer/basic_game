f85749d84529a6b589ccce7a4fdbdf684fd25177
W. Brad Meyer <wbradmeyer@gmail.com>
2025-05-18 13:39:59 -0400

    add comments in players.js

--- game/players.js
+++ game/players.js
@@ -1,3 +1,4 @@
+// The Player class represents a player in the game
 export class Player {
     constructor(name, setGameBoardCallback) {
         this.name = name
@@ -9,21 +10,23 @@ export class Player {
     
     attack(enemy){
         // if weapon accuracy hits, deal damage, else skip
+        // Reduce the enemy's health by the damage of the player's weapon
         enemy.health -= this.weapon.damage
         this.setGameBoard()
     }
     restoreHealth(){
+        // Increase the player's health by the restore value of the equipped potion
         this.health += this.potion.restore
         this.setGameBoard()
     }
     
     getWeapon(weapon){
-        this.weapon = weapon
+        this.weapon = weapon // Assign the weapon to the player
         this.setGameBoard()
     }
 
     getPotion(potion){
-        this.potion = potion
+        this.potion = potion // Assign the weapon to the player
         this.setGameBoard()
     }
 }
